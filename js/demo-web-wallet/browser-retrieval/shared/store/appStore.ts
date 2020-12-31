379e8c520b8fca53824843b85cfd5793a7b31545
Jim Pick <jim@jimpick.com>
2020-12-30 19:50:17 -0800

    Initial version of web demo

---
+++ js/demo-web-wallet/browser-retrieval/shared/store/appStore.ts
@@ -0,0 +1,122 @@
+import { makeAutoObservable } from 'mobx'
+// import { Node } from 'shared/Node'
+
+// import { AlertsStore } from './alertsStore'
+// import { DealsStore } from './dealsStore'
+import { LogsStore } from './logsStore'
+// import { OffersStore } from './offersStore'
+// import { OperationsStore } from './operationsStore'
+import { OptionsStore } from './optionsStore'
+// import { QueriesStore } from './queriesStore'
+// import { RecentCIDStore } from './recentCIDStore'
+import { SettingsStore } from './settingsStore'
+// import { UploadStore } from './uploadStore'
+// import { PCHStore } from './pchStore'
+
+export class AppStore {
+  // alertsStore: AlertsStore
+  // dealsStore: DealsStore
+  logsStore: LogsStore
+  // offersStore: OffersStore
+  // operationsStore: OperationsStore
+  optionsStore: OptionsStore
+  // queriesStore: QueriesStore
+  // recentCIDStore: RecentCIDStore
+  settingsStore: SettingsStore
+  // uploadStore: UploadStore
+  // pchStore: PCHStore
+
+  // node: Node
+
+  connected = false
+
+  // connectedPeers = []
+
+  constructor() {
+    // this.alertsStore = new AlertsStore(this)
+    // this.dealsStore = new DealsStore(this)
+    this.logsStore = new LogsStore(this)
+    // this.offersStore = new OffersStore(this)
+    // this.operationsStore = new OperationsStore(this)
+    this.optionsStore = new OptionsStore(this)
+    // this.queriesStore = new QueriesStore(this)
+    // this.recentCIDStore = new RecentCIDStore(this)
+    this.settingsStore = new SettingsStore(this)
+    // this.uploadStore = new UploadStore(this)
+    // this.pchStore = new PCHStore(this)
+
+    makeAutoObservable(this)
+  }
+
+  async connect() {
+    const { optionsStore } = this
+
+    if (optionsStore.wallet || optionsStore.privateKey) {
+      try {
+        await this.tryDisconnect()
+
+        // this.node = await Node.create(true)
+        // this.connected = true
+
+        /*
+        const mimBalance = await this.lotus.hasMinBalance()
+
+        if (!mimBalance) {
+          console.error('Mimbalance')
+          this.alertsStore.create({
+            message: 'Your wallet does not have minimum FIL required!',
+            type: 'warning',
+          })
+        }
+        */
+      } catch (error) {
+        this.connected = false
+
+        if (error === 'Error: `Invalid Key Length`') {
+          this.logsStore.logDebug(`start node failed: ${error}`)
+          console.error(error)
+        } else {
+          console.error(error)
+          this.logsStore.logError(`start node failed: ${error.message}`)
+        }
+      }
+    }
+  }
+
+  private async tryDisconnect() {
+    try {
+      // await this.node?.stop()
+      this.connected = false
+    } catch (err) {
+      this.logsStore.logDebug(`stop node failed: ${err.message}`)
+    }
+  }
+
+  /*
+  query({ cid, minerID }) {
+    this.node.query(cid, minerID)
+  }
+
+  setConnectedPeers(connectedPeers) {
+    this.connectedPeers = connectedPeers
+  }
+
+  downloadFile(msg: { cid: any; offer?: any }) {
+    this.node.downloadFile(msg)
+  }
+
+  deleteFile({ cid }) {
+    this.node.deleteFile(cid)
+  }
+
+  stopAutomationCode() {
+    this.node.stopLoop()
+  }
+
+  runAutomationCode() {
+    this.node.runAutomationCode()
+  }
+  */
+}
+
+export const appStore = new AppStore()
---
+++ js/demo-web-wallet/webpack.config.js
@@ -0,0 +1,48 @@
+// based on https://webpack.js.org/guides/development/
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    index: './index-playwright.ts'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+    fallback: { "util": require.resolve("util/") }
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    }),
+    new AddAssetHtmlPlugin({ filepath: require.resolve('./wasm_exec.js') }),
+    new Dotenv(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  experiments: {
+    syncWebAssembly: true,
+  }
+}
